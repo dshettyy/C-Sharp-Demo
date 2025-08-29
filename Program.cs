4b31499b311a93e649c075a00a346500b09e905c
dshettyy <dishanthshettyy@gmail.com>
2025-08-29 10:23:08 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,28 +1,66 @@
 ﻿using System;
+using System.Xml.Serialization;
 
-class Age
+namespace sub
 {
-    private int age ;
-
-    public void Takeage()
+    class Shape
     {
-        Console.Write("Enter age: ");
-        age = Convert.ToInt32(Console.ReadLine());
+        public virtual void Draw()
+        {
+            Console.WriteLine("Drawing a Shape");
+        }
+          public virtual void Erase()
+        {
+            Console.WriteLine("Erasing a Shape");
+        }  
     }
-        public void Showage()
+    class Circle : Shape
     {
-            Console.WriteLine("Age is " + age);
-    }
-}
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing a circle");
+        }
+        public override void Erase()
+        {
+            Console.WriteLine("Erasing a circle");
+        }
 
-class myclass
-{
-    static void Main(string[] args)
+    }
+    class Triangle : Shape
     {
-        Age a=new Age();
-
-        a.Takeage();
-        a.Showage();
-        
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing a triangle");
+        }
+        public override void Erase()
+        {
+            Console.WriteLine("Erasing a Triangle");
+        }
     }
+    class Square : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing a Square");
+        }
+        public override void Erase()
+        {
+            Console.WriteLine("Erasing a Square");
+        }
+
+    }
+    class main
+    {
+        public static void Main(string[] args)
+        {
+            Shape[] shape = { new Circle(), new Triangle(), new Square() };
+
+            foreach (Shape s in shape)
+            {
+                s.Draw();
+                s.Erase();
+            }
+        }
+    }
+
 }
