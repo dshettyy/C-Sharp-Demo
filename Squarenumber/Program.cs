399a5577f6ec915bed4746663e8a9d1fd3025b7e
dshettyy <dishanthshettyy@gmail.com>
2025-09-05 09:30:53 +0530

    Initial commit

---
+++ Squarenumber/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Squarenumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Square s = new Square();
+            s.cal();
+            s.display();
+        }
+    }
+}
---
+++ Squarenumber/Square.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Squarenumber
+{
+    internal class Square
+    {
+        int num;
+        int square;
+        public Square()
+        {
+            num = 2;
+
+        }
+        public Square(int num_)
+        { 
+            this.num = num_;
+        }
+        public void cal() {
+            square = num * num;
+}
+        public void display() {
+
+            Console.WriteLine("the Square of "+ num+" is " + square);
+            }
+    }
+}
