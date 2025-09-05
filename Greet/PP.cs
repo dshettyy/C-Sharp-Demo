d12adb8dc91c2b69701b3d5b097f5d205b5dd4ce
dshettyy <dishanthshettyy@gmail.com>
2025-09-05 09:30:20 +0530

    Add files via upload

---
+++ Greet/PP.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greet
+{
+    public class PP
+    {
+        string name;
+        public PP()
+            {
+            name = "sun Goku";
+                }
+        public PP(string name)
+        {
+            this.name = name;
+        }
+
+            public void greet()
+            {
+                Console.WriteLine($"Hello {name}");
+            }
+    }
+
+    }
---
+++ Greet/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Greet
+{
+ public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the name:");
+            String name = Console.ReadLine();
+            PP p = new PP(name);
+            p.greet();
+
+        }
+    }
+}
