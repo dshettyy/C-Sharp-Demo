a0195fa9255f5ca5a4faa480ea55d1aa6b17d934
dshettyy <dishanthshettyy@gmail.com>
2025-09-19 10:04:32 +0530

    Initial Commit

---
+++ Expection_Handeling/Expection_Handeling/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Expection_Handeling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\tException Handeling");
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            Console.WriteLine("Enter number to be divided :");
+            int a = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Enter number to divide :");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int result;
+            try
+            {
+                result = a / b;
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("Result = " + result);
+            }
+            catch (DivideByZeroException
+            )
+            {
+                Console.WriteLine("Number cannot be divided by zero");
+            }
+
+            finally
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+               
+                Console.WriteLine("Division Process completed");
+            }
+
+        }
+    }
+}
