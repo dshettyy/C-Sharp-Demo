3afb2d5cb8102148fd3921d53a74ab10c8d081e6
dshettyy <dishanthshettyy@gmail.com>
2025-09-19 10:19:11 +0530

    Initial Commit

---
+++ Multiple_Devices/Multiple_Devices/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Multiple_Devices
+{
+    interface ICamera
+    {
+        void Capture();
+    }
+
+    interface IPrinter
+    {
+        void Print();
+    }
+
+    class Computer : ICamera, IPrinter
+    {
+        public void Capture()
+        {
+            Console.WriteLine("Camera captures photo");
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Printer prints");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Computer myComputer = new Computer();
+
+            myComputer.Capture(); 
+            myComputer.Print();   
+        }
+    }
+}
